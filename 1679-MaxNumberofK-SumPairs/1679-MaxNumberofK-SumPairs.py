3b32f73ccc854b67eb7475168d878ae6cdf8b979
James Siddeley <123500463+jamessidd@users.noreply.github.com>
2025-01-28 13:45:39 -0500

    Create file

---
+++ 1679-MaxNumberofK-SumPairs/1679-MaxNumberofK-SumPairs.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        while left < right:
+            # print(nums[left], nums[right])
+            if nums[left] + nums[right] == k:
+                count += 1
+                left += 1
+                right -= 1
+            elif nums[left] + nums[right] < k:
+                left += 1
+            else:
+                right -= 1
+        return count
+
+        
