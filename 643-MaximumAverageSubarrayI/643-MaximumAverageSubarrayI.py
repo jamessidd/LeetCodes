ec02699a0fb5e524e4dc5cbcbc14fabbf7d6cd24
James Siddeley <123500463+jamessidd@users.noreply.github.com>
2025-01-28 13:53:21 -0500

    Create file

---
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        currsum = sum(nums[:k])
+        maxsum = currsum
+        print(currsum)
+        for i in range(k, len(nums)):
+
+            currsum -= nums[i - k]
+            currsum += nums[i]
+            maxsum = max(maxsum, currsum)
+
+        return maxsum / float(k)
