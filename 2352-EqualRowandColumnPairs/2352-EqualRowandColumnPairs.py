e64094bf3c4b5197ef2a58dc5f6ae555b198f07b
James Siddeley <123500463+jamessidd@users.noreply.github.com>
2025-01-28 21:13:34 -0500

    Create file

---
+++ 2352-EqualRowandColumnPairs/2352-EqualRowandColumnPairs.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def equalPairs(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        rdict = defaultdict(list)
+        cdict = defaultdict(list)
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+
+                rdict[row].append(grid[row][col])
+                cdict[col].append(grid[row][col])
+
+        print(rdict)
+        print(cdict)
+
+        count = 0
+        for rows in rdict.values():
+
+            for cols in cdict.values():
+
+                if rows == cols:
+                    count +=1
+        
+        return count
+
+            
